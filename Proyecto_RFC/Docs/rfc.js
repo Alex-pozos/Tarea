e1878b13c6f00c85ed58daa073b8a87789782900
Alex-pozos <66715433+Alex-pozos@users.noreply.github.com>
2021-04-18 14:21:33 -0500

    RFC

--- Proyecto_RFC/Docs/rfc.js
+++ Proyecto_RFC/Docs/rfc.js
@@ -1,7 +1,7 @@
 //Primero creamos las variables ya inicializadas que ocuparemos para echar a andar nuestro programa, para que sea mas como todas seran de tipo String
-var nombre = "Alejandro";
-var ap_paterno = "Pozos";
-var ap_materno = "Rivera";
+var nombre = "Maria Ofelia";
+var ap_paterno = "Peredo";
+var ap_materno = "Dominguez";
 var fecha = "15-03-1956";
 var rfc = "";
 /*for (let index = 0; index < fecha.length; index++) {
@@ -16,9 +16,15 @@ function caracter_invalido(letra) {
 function quita_primer_nom(palabra) {
     return palabra.replace("MARIA", "").replace("MA.", "").replace("MA", "").replace("JOSE", "").replace("J", "").replace("J.", "").trim();
 }
-//La siguiente funcion quita todos los articulos cuando el apellido tiene DEL, LAS, DE, LA, Y, A a lo cual por medio del metodo replace lo cambia por un espacio en blanco
+/*La siguiente funcion quita todos los articulos cuando el apellido tiene DEL, LAS, DE, LA, Y, A a lo cual por medio del metodo replace lo cambia por un espacio en
+blanco*/
 function quita_articulos(palabra) {
     return palabra.replace("DEL ", "").replace("LAS ", "").replace("DE ", "").replace("LA ", "").replace("Y ", "").replace("A ", "");
+}
+/*La siguiente funcion quita todos los articulos cuando el apellido tiene DA, DAS, DE, DEL, DER, DI, DIE, DD, EL, LA, LOS, LAS, LE, LES, MAC, MC, VAN, VON, Y
+a lo cual por medio del metodo replace lo cambia por un espacio en blanco*/
+function quita_preposicion(palabra) {
+    return palabra.replace("DA", "").replace("DAS", "").replace("DE", "").replace("DEL", "").replace("DER", "").replace("DI", "").replace("DIE", "").replace("DD", "").replace("EL", "").replace("LA", "").replace("LAS", "").replace("LE", "").replace("LES", "").replace("MAC", "").replace("MC", "").replace("VAN", "").replace("VON", "").replace("Y", "").trim();
 }
 //Para empezar a los apellidos tanto materno como paterno les quitamos los articulos por medio de la funcion antes ya declarada
 ap_materno = quita_articulos(ap_materno.toUpperCase());
@@ -43,6 +49,25 @@ else if (nombre_x_partes[0] != "MARIA" || nombre_x_partes[0] != "MA." || nombre_
     nombre_x_partes[0] != "JOSE" || nombre_x_partes[0] != "J" || nombre_x_partes[0] != "J.") {
     nombre.replace(" ", "");
 }
+/*Aqui creamos una nueva variable que sera un array de tipo String donde le asignaremos el nombre en mayusculas partido con el metodo split por si encuentra un
+segundo Apellido en materno y paterno */
+var apellidop_x_partes = ap_paterno.toUpperCase().split(" ");
+var apellidom_x_partes = ap_materno.toUpperCase().split(" ");
+/*Aqui creamos una nueva variable que sera un array de tipo String donde le asignaremos los datos que no se pueden ingresar y posteriormente compararlos */
+/*DA, DAS, DE, DEL, DER, DI, DIE, DD, EL, LA, LOS, LAS, LE, LES, MAC, MC, VAN, VON, Y */
+var no_valido = ["DA", "DAS", "DE", "DEL", "DER", "DI", "DIE", "DD", "EL", "LA", "LOS", "LAS", "LE", "LES", "MAC", "MC", "VAN", "VON", "Y"];
+/*Aqui leemos todos los datos que tenga el array que creamos  */
+for (var index = 0; index < no_valido.length; index++) {
+    /*Comparamos todos los datos para que no haya datos invalidos*/
+    if (apellidop_x_partes[0] != no_valido[index] || apellidom_x_partes[0] != no_valido[index]) {
+        ap_paterno = quita_preposicion(ap_paterno.toUpperCase());
+        ap_materno = quita_preposicion(ap_materno.toUpperCase());
+    }
+    else {
+        ap_paterno.replace(" ", "");
+        ap_materno.replace(" ", "");
+    }
+}
 //Aqui mande a imprimir por medio de un consol por que no sabia como me regresaria la fecha 
 console.log(fecha.split("-"));
 /*Nuevamente por medio de la funcion split cortamos la cadena para obtener lo que nececitamos*/
@@ -59,5 +84,23 @@ rfc += nombre.substr(0, 1).toUpperCase();
 rfc += ano.substr(2, 4);
 rfc += mes.substr(0, 2);
 rfc += dia.substr(0, 2);
+/*Agregamos un nuevo array de tipo String donde le asignaremos las palabras antisonantes  */
+var palabras_feas = ["BACA", "BAKA", "BUEI", "BUEY", "CACA", "CACO", "CAGA", "CAGO", "CAKA", "CAKO", "COGE", "COGI", "COJA", "COJE", "COJI", "COJO", "PUTO", "CACA", "PENE",
+    "NEPE", "PIPI", "PEDO"];
+/*Por medio de la siguiente estructura de repeticion nos apoyaremos para poder leer todas las palabras feas */
+for (var index = 0; index < palabras_feas.length; index++) {
+    /*Con la siguiente estructura condicional validaremos que en el tramo de cadena que vamos a obtener por medio del substr, si es igual a una palabra antisonante
+    elimine el espacio de la palanra 2 y lo remplace por una "X" */
+    if (rfc.substr(0, 4) == palabras_feas[index]) {
+        rfc = rfc.slice(0, 1);
+        rfc += "X";
+        rfc += ap_materno.substr(0, 1).toUpperCase();
+        rfc += nombre.substr(0, 1).toUpperCase();
+        rfc += ano.substr(2, 4);
+        rfc += mes.substr(0, 2);
+        rfc += dia.substr(0, 2);
+    }
+}
 /*Y mandamos a imprimir nuestra variable por medio del consol.log*/
-console.log(rfc);
+console.info("Nombre: " + nombre.toUpperCase() + "\nApellido Paterno: " + ap_paterno + "\nApellido Materno: " + ap_materno + "\nFecha de nacimiento: " + fecha);
+console.warn("Tu RFC es: " + rfc);
--- Proyecto_RFC/Docs/rfc.ts
+++ Proyecto_RFC/Docs/rfc.ts
@@ -1,9 +1,10 @@
 //Primero creamos las variables ya inicializadas que ocuparemos para echar a andar nuestro programa, para que sea mas como todas seran de tipo String
-let nombre:String = "Alejandro";
-let ap_paterno:String = "Pozos";
-let ap_materno:String = "Rivera";
+let nombre:String = "Maria Ofelia";
+let ap_paterno:String = "Peredo";
+let ap_materno:String = "Dominguez";
 let fecha:String = "15-03-1956";
 let rfc:String = "";
+
 /*for (let index = 0; index < fecha.length; index++) {
     const element = fecha[index];
     
@@ -18,10 +19,18 @@ function quita_primer_nom(palabra:any){
         ,"").replace("J","").replace("J.","").trim();    
     
 }
-//La siguiente funcion quita todos los articulos cuando el apellido tiene DEL, LAS, DE, LA, Y, A a lo cual por medio del metodo replace lo cambia por un espacio en blanco
+/*La siguiente funcion quita todos los articulos cuando el apellido tiene DEL, LAS, DE, LA, Y, A a lo cual por medio del metodo replace lo cambia por un espacio en 
+blanco*/
 function quita_articulos(palabra:any) {
     return palabra.replace("DEL ", "").replace("LAS ", "").replace("DE ",
             "").replace("LA ", "").replace("Y ", "").replace("A ", "");
+}
+/*La siguiente funcion quita todos los articulos cuando el apellido tiene DA, DAS, DE, DEL, DER, DI, DIE, DD, EL, LA, LOS, LAS, LE, LES, MAC, MC, VAN, VON, Y
+a lo cual por medio del metodo replace lo cambia por un espacio en blanco*/
+function quita_preposicion(palabra:any){
+    return palabra.replace("DA","").replace("DAS","").replace("DE","").replace("DEL","").replace("DER","").replace("DI","").replace("DIE","").replace("DD",
+    "").replace("EL","").replace("LA","").replace("LAS","").replace("LE","").replace("LES","").replace("MAC","").replace("MC","").replace("VAN","").replace("VON",
+    "").replace("Y","").trim();
 }
 //Para empezar a los apellidos tanto materno como paterno les quitamos los articulos por medio de la funcion antes ya declarada
 ap_materno = quita_articulos(ap_materno.toUpperCase());
@@ -46,16 +55,33 @@ if (nombre_x_partes[0] == "MARIA" || nombre_x_partes[0] == "MA." || nombre_x_par
 nombre_x_partes[0] != "JOSE" || nombre_x_partes[0] != "J" || nombre_x_partes[0] != "J.") {
     nombre.replace(" ","");
 }
+/*Aqui creamos una nueva variable que sera un array de tipo String donde le asignaremos el nombre en mayusculas partido con el metodo split por si encuentra un 
+segundo Apellido en materno y paterno */
+let apellidop_x_partes:String[] = ap_paterno.toUpperCase().split(" ");
+let apellidom_x_partes:String[] = ap_materno.toUpperCase().split(" ");
+/*Aqui creamos una nueva variable que sera un array de tipo String donde le asignaremos los datos que no se pueden ingresar y posteriormente compararlos */
+/*DA, DAS, DE, DEL, DER, DI, DIE, DD, EL, LA, LOS, LAS, LE, LES, MAC, MC, VAN, VON, Y */
+let no_valido:String[] = ["DA", "DAS", "DE", "DEL", "DER", "DI", "DIE", "DD", "EL", "LA", "LOS", "LAS", "LE", "LES", "MAC", "MC", "VAN", "VON", "Y"];
+/*Aqui leemos todos los datos que tenga el array que creamos  */
+for (let index = 0; index < no_valido.length; index++) {
+    /*Comparamos todos los datos para que no haya datos invalidos*/
+    if (apellidop_x_partes[0] != no_valido[index] || apellidom_x_partes[0] != no_valido[index]) {
+        ap_paterno = quita_preposicion(ap_paterno.toUpperCase());
+        ap_materno = quita_preposicion(ap_materno.toUpperCase());
+    }else{
+        ap_paterno.replace(" ","");
+        ap_materno.replace(" ","");
+    }    
+    
+}
 //Aqui mande a imprimir por medio de un consol por que no sabia como me regresaria la fecha 
 console.log(fecha.split("-"));
-
 /*Nuevamente por medio de la funcion split cortamos la cadena para obtener lo que nececitamos*/
 let fecha_x_partes:String[]= fecha.split("-");
 /*Aqui creamos nuevas variables donde asignaremos dia, mes y año que son los datos que nos interesan*/
 let dia:String = fecha_x_partes[0];
 let mes:String = fecha_x_partes[1];
 let ano:String = fecha_x_partes[2];
-
 /*Por ultimo agregamos los datos a la variable rfc con '+=' para evitarnos mas lineas de codigo, y le asignamos por medio del metodo substr los fragmentos de cadena
 que nos importa en realidad de cada uno de los valores */
 rfc += ap_paterno.substr(0,2).toUpperCase();
@@ -64,6 +90,23 @@ rfc += nombre.substr(0,1).toUpperCase();
 rfc += ano.substr(2,4);
 rfc += mes.substr(0,2);
 rfc += dia.substr(0,2);
-
+/*Agregamos un nuevo array de tipo String donde le asignaremos las palabras antisonantes  */
+let palabras_feas:String[] = ["BACA","BAKA","BUEI","BUEY","CACA","CACO","CAGA","CAGO","CAKA","CAKO","COGE","COGI","COJA","COJE","COJI","COJO","PUTO", "CACA", "PENE", 
+"NEPE", "PIPI","PEDO"];
+/*Por medio de la siguiente estructura de repeticion nos apoyaremos para poder leer todas las palabras feas */
+for (let index = 0; index < palabras_feas.length; index++) {
+    /*Con la siguiente estructura condicional validaremos que en el tramo de cadena que vamos a obtener por medio del substr, si es igual a una palabra antisonante
+    elimine el espacio de la palanra 2 y lo remplace por una "X" */
+    if (rfc.substr(0,4) == palabras_feas[index]) {
+        rfc = rfc.slice(0,1);
+        rfc += "X";
+        rfc += ap_materno.substr(0,1).toUpperCase();
+        rfc += nombre.substr(0,1).toUpperCase();
+        rfc += ano.substr(2,4);
+        rfc += mes.substr(0,2);
+        rfc += dia.substr(0,2);
+    }
+}
 /*Y mandamos a imprimir nuestra variable por medio del consol.log*/
-console.log(rfc);
+console.info("Nombre: "+nombre.toUpperCase()+"\nApellido Paterno: "+ap_paterno+"\nApellido Materno: "+ap_materno+"\nFecha de nacimiento: "+fecha);
+console.warn("Tu RFC es: "+rfc);
